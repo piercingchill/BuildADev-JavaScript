642d6c4718d4c27666da1be69619e7414929fcc2
piercingchill <103719211+piercingchill@users.noreply.github.com>
2023-05-28 21:42:48 -0400

    Delete 2. Node directory
    Not needed

--- 2. Node/Node-Exercises/Node-Exercise-3/send.js
+++
@@ -1,9 +0,0 @@
-// Exercise 3: Sending Data in the Request Body
-
-// Step 1: Import the necessary modules.
-// Step 2: Define the URL of the server you want to send a POST request to.
-// Step 3: Prepare the data to be sent in the request body (e.g., using JSON.stringify).
-// Step 4: Use the http module to create a POST request to the specified URL.
-// Step 5: Set up event listeners for the response event and the data event.
-// Step 6: As data is received, concatenate it into a response string.
-// Step 7: When the response ends, log the complete response to the console.
